e71d4ef542899499e9b8d315a89858ae8515e611
ChandrakantPal <sanjay161097@gmail.com>
2023-07-05 03:16:45 +0530

    Icons components

---
+++ src/components/Icons/CloseIcon.tsx
@@ -0,0 +1,15 @@
+const CloseIcon = () => {
+  return (
+    <svg
+      className="w-6 h-6"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <path d="M6 18L18 6M6 6l12 12" />
+    </svg>
+  );
+};
+
+export default CloseIcon;
