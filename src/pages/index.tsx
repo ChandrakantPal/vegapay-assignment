9679172c9f0d81271088f789c23b00f5f2899c69
ChandrakantPal <sanjay161097@gmail.com>
2023-07-05 03:16:26 +0530

    card sales page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,164 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import * as React from "react";
+import CardIcon from "@/components/Icons/CardIcon";
+import ReloadIcon from "@/components/Icons/ReloadIcon";
+import UserIcon from "@/components/Icons/UserIcon";
+import Tabs, { TabsType } from "@/components/Tabs/Tabs";
+import { PaginationType, SearchedResultType } from "@/types/user";
+import SearchIcon from "@/components/Icons/SearchIcon";
+import Button from "@/components/Button";
+import TabContent from "@/components/Tabs/TabContent";
+import LoadingTable from "@/components/LoadingTable";
 
-const inter = Inter({ subsets: ['latin'] })
+const tabsArray: TabsType = [
+  {
+    id: "kit",
+    title: "Kit",
+    icon: <CardIcon />,
+  },
+  {
+    id: "personalizedSales",
+    title: "Personalized Sales",
+    icon: <UserIcon />,
+  },
+  {
+    id: "reissue",
+    title: "Reissue",
+    icon: <ReloadIcon />,
+  },
+];
+
+const fetchConfig = {
+  headers: {
+    Authorization:
+      "Bearer  eyJraWQiOiJnWlwvclBnV1RLVjBWbUFpWW52WVwvbUNaZUdxNXpHVVJDUWlpU0FUaHZyVjQ9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI2NDJlMTNjYS0yMmYzLTQ4ZjktYWMzMS02ZWE2ZGYzY2YwZDkiLCJldmVudF9pZCI6IjgyNGVmMzBiLWE3MjQtNDFiNi1hZjI5LWEyYzAzNGNlODE5NSIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2ODgzODM1NDcsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1zb3V0aC0xLmFtYXpvbmF3cy5jb21cL2FwLXNvdXRoLTFfQlFqQWVQbWVkIiwiZXhwIjoxNjg4NTYzODcxLCJpYXQiOjE2ODg0Nzc0NzEsImp0aSI6IjkxZTkxYmI0LThkMTgtNDk3Ny1iOThkLTU3NTAzMTg4ODFiNiIsImNsaWVudF9pZCI6IjdqOWJyOTIxYWZmMGNzcHNlNnBqaGMydnA3IiwidXNlcm5hbWUiOiI2NDJlMTNjYS0yMmYzLTQ4ZjktYWMzMS02ZWE2ZGYzY2YwZDkifQ.ex6urzex-jxHYf4o0OVrZmE-7eaFP-yV5PMzpGdqmECLvQOsFSf3JYX8vySvE1UtmIx4j7G5kFPOtcrDBOJnZk7CXscQtUCf7VGAFsx8LifarM2eA5-1GoISPjUtaaF2tbU2cvsKTB_lcUJw-Koh3jbTMo_oiZuzn-2ZtT3sZ9ok99uN28k5DPduf3m_J-GnsAtuuq-ns3jCtznxOfDoaVNctmqerwz-yOIhyQWjHnF9WH6yzhcZnoM09dJBv43uIZzE7utMDfzP2Et_MGUohfQspsD1xSZa3fvnEe4RBAHkeUfVaIgDXN985YmVqdPp_fWv3rywz1R8wiydUzcVmA",
+    "Content-Type": "application/json",
+  },
+};
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [activeTab, setActiveTab] = React.useState("kit");
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const [debouncedSearchQuery, setDebouncedSearchQuery] = React.useState("");
+  const [data, setData] = React.useState<SearchedResultType[]>([]);
+  const [pagination, setPagination] = React.useState<PaginationType>({
+    pageNumber: 0,
+    numberOfItems: 0,
+    totalPages: 0,
+    totalItems: 0,
+  });
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+
+  const [currentPage, setCurrentPage] = React.useState(0);
+
+  const searchUsers = React.useCallback(async () => {
+    try {
+      setLoading(true);
+      setError("");
+      const payload = {
+        value: debouncedSearchQuery,
+        branchId: "4ff819ab-00ea-45b8-9544-205407c0680c",
+        page: 0,
+        pageSize: 50,
+      };
+      const response = await fetch(
+        "https://prod-api.vegapay.tech/forex/account/search",
+        {
+          ...fetchConfig,
+          method: "POST",
+          body: JSON.stringify(payload),
+        }
+      );
+      const data = await response.json();
+      setData(data.records);
+      setPagination({
+        pageNumber: data.pageNumber,
+        numberOfItems: data.numberOfItems,
+        totalPages: data.totalPages,
+        totalItems: data.totalItems,
+      });
+      setLoading(false);
+    } catch (e) {
+      setError("Something went wrong");
+      setLoading(false);
+    }
+  }, [debouncedSearchQuery]);
+
+  React.useEffect(() => {
+    searchUsers();
+  }, [searchUsers]);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedSearchQuery(searchQuery);
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  return (
+    <section className={`min-h-screen space-y-4`}>
+      <div className="space-y-4 card">
+        <div>
+          <h6 className="text-base">New Card Sales</h6>
+          <div className="text-sm text-gray-600">Issue New Card</div>
+        </div>
+        <Tabs
+          tabs={tabsArray}
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="flex items-center justify-between card">
+        <div className="relative flex items-center">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <SearchIcon />
+          </div>
+          <input
+            type="text"
+            className="block w-full py-2 pl-10 pr-4 border-gray-300 rounded-md focus:border-blue-500 focus:ring-blue-500"
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+            placeholder="Search by Mobile"
+          />
+        </div>
+        <Button
+          variant="outlined"
+          color="primary"
+          type="button"
+          onClick={searchUsers}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Search
+        </Button>
       </div>
-    </main>
-  )
+      {loading ? (
+        <LoadingTable />
+      ) : (
+        <>
+          {error && (
+            <div className="text-xl text-center text-red-500">{error}</div>
+          )}
+          {data.length > 0 && (
+            <TabContent
+              data={data}
+              pagination={pagination}
+              setCurrentPage={setCurrentPage}
+              activeTab={activeTab}
+            />
+          )}
+          {data.length === 0 && (
+            <div className="text-xl text-center text-gray-400">
+              No Data Found
+            </div>
+          )}
+          {}
+        </>
+      )}
+    </section>
+  );
 }
